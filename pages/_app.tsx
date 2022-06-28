b6343018c7423854be869c34986b0710a7c07705
Ugo Romi <ugo.romi@gmail.com>
2022-06-28 20:19:58 +0200

    Added proof calcolation Improved styling Added mongodb connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-stone-800 w-screen h-screen text-stone-200 max-w-full selection:bg-amber-300'>
+      <Component {...pageProps} />
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
