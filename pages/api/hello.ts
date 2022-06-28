27829760e08f58733093a1b322ac2baf7ccf1f5f
Ugo Romi <ugo.romi@gmail.com>
2022-06-28 20:21:09 +0200

    Commented broken import

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from '@/lib/mongodb';
+//import clientPromise from '@/lib/mongodb';
 
 type Data = {
   name: string;
